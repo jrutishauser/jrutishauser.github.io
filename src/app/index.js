68a00289c2126f79dda1a9ef2ce2a69e46221d0c
Jon Rutishauser <jrutishauser@gmail.com>
2015-04-01 19:50:57 -0400

    updates to each page

--- src/app/index.js
+++ src/app/index.js
@@ -7,8 +7,26 @@ angular.module('jrutishausergithubio', ['ngSanitize', 'ui.router'])
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
-      });
-
+      })
+	  .state('home.about', {
+	  	url: '/about',
+	  	templateUrl: 'app/about/about.html',
+	  	controller: 'AboutCtrl',
+	  	controllerAs: 'about'
+	  })
+	  .state('home.work', {
+	  	url: '/work',
+	  	templateUrl: 'app/work/work.html',
+	  	controller: 'WorkCtrl',
+	  	controllerAs: 'work'
+	  })
+	  .state('home.blog', {
+	  	url: '/blog',
+	  	templateUrl: 'app/blog/blog.html',
+	  	controller: 'BlogCtrl',
+	  	controllerAs: 'blog'
+	  })
+	  ;//end states
     $urlRouterProvider.otherwise('/');
   })
 ;
---
+++ src/app/work/work.controller.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('jrutishausergithubio')
+  .controller('WorkCtrl', function () {
+  });
